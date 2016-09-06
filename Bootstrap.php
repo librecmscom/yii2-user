daac019534c5d02471bb46e7a6aa477d9dec4918
xutl <79109681@qq.com>
2016-09-06 18:22:39 +0800

    init

---
+++ Bootstrap.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+namespace yuncms\user;
+
+use yii\web\GroupUrlRule;
+use yii\i18n\PhpMessageSource;
+
+/**
+ * Class Bootstrap
+ * @package yuncms/user
+ */
+class Bootstrap extends BootstrapInterface
+{
+    public function bootstrap($app)
+    {
+        /** @var Module $module */
+        /** @var \yii\db\ActiveRecord $modelName */
+        if ($app->hasModule('user') && ($module = $app->getModule('user')) instanceof Module) {
+            if ($app instanceof \yii\console\Application) {
+                $app->controllerMap['user'] = [
+                    'class' => 'yuncms\user\console\UserController',
+                ];
+            } else if ($module instanceof \yuncms\user\backend\Module) {
+
+            } else if ($module instanceof \yuncms\user\api\Module) {
+
+            } elseif ($module instanceof Module) {//前台判断放最后
+                $configUrlRule = [
+                    'prefix' => $module->urlPrefix,
+                    'rules' => $module->urlRules,
+                ];
+                if ($module->urlPrefix != 'user') {
+                    $configUrlRule['routePrefix'] = 'user';
+                }
+                $app->urlManager->addRules([new GroupUrlRule($configUrlRule)], false);
+            }
+        }
+        /**
+         * 注册语言包
+         */
+        if (!isset($app->get('i18n')->translations['user*'])) {
+            $app->get('i18n')->translations['user*'] = [
+                'class' => PhpMessageSource::className(),
+                'basePath' => __DIR__ . '/messages',
+            ];
+        }
+    }
+}
---
+++ Module.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+namespace yuncms\user;
+
+class Module extends \yii\base\Module
+{
+    
+}
