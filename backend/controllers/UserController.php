31579974554d3bf1fb47f103abf46417c40cb308
xutl <79109681@qq.com>
2016-09-07 11:37:13 +0800

    ccc

---
+++ backend/controllers/UserController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+namespace yuncms\user\backend\controllers;
+
+use yii\web\Controller;
+
+/**
+ * Class UserController
+ * @package yuncms
+ */
+class UserController extends Controller
+{
+
+}
