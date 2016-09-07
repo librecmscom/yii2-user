c19d0f27f15eb0a87749efbf397ab9100ab17baa
xutl <79109681@qq.com>
2016-09-07 11:39:22 +0800

    update

--- backend/models/UserSettingForm.php
+++ backend/models/UserSettingForm.php
@@ -4,7 +4,7 @@
  * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
  * @license http://www.tintsoft.com/license/
  */
-namespace backend\models;
+namespace yuncms\user\backend\models;
 
 use yii\base\Model;
 
