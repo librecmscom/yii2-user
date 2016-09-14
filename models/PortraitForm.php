0c64e7f70ddb7a1d83580eece330c1ef3f4b0b65
xutl <79109681@qq.com>
2016-09-14 17:08:44 +0800

    cc

---
+++ models/PortraitForm.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+namespace yuncms\user\models;
+
+use Yii;
+use yii\base\Model;
+
+/**
+ * Class PortraitForm
+ * @package Leaps\User\Model
+ * @author Xu Tongle <xutongle@gmail.com>
+ * @since 3.0
+ */
+class PortraitForm extends Model
+{
+    /**
+     * @var \yii\web\UploadedFile 头像上传字段
+     */
+    public $portrait;
+
+    /** @var Profile */
+    private $_profile;
+
+    /** @return User */
+    public function getProfile()
+    {
+        if ($this->_profile == null) {
+            $this->_profile = Yii::$app->user->identity->profile;
+        }
+        return $this->_profile;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['portrait'], 'required'],
+            [['portrait'], 'file', 'extensions' => 'gif, jpg, png', 'maxSize' => 1024 * 1024 * 2, 'tooBig' => Yii::t('app', 'File has to be smaller than 2MB')],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'portrait' => Yii::t('user', 'Portrait'),
+        ];
+    }
+
+    /**
+     * 保存头像
+     *
+     * @return boolean
+     */
+    public function upload()
+    {
+        if ($this->validate()) {
+            $profile = $this->getProfile();
+            /** @var \Yii\System\Module $module */
+            $module = Yii::$app->getModule('system');
+            if ($module->saveAvatar($profile->user_id, $this->portrait->tempName)) {
+                $profile->avatar = true;
+                $profile->save();
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ models/ResendForm.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+namespace yuncms\user\models;
+
+use Yii;
+use yii\base\Model;
+use yuncms\user\ModuleTrait;
+
+/**
+ * ResendForm gets user email address and validates if user has already confirmed his account. If so, it shows error
+ * message, otherwise it generates and sends new confirmation token to user.
+ *
+ * @property User $user
+ */
+class ResendForm extends Model
+{
+    use ModuleTrait;
+
+    /**
+     * @var string
+     */
+    public $email;
+
+    /**
+     * @var User
+     */
+    private $_user;
+
+
+    /**
+     * @return User
+     */
+    public function getUser()
+    {
+        if ($this->_user === null) {
+            $this->_user = User::findByEmail($this->email);
+        }
+
+        return $this->_user;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            'emailRequired' => ['email', 'required'],
+            'emailPattern' => ['email', 'email'],
+            'emailExist' => ['email', 'exist', 'targetClass' => User::className()],
+            'emailConfirmed' => ['email', function () {
+                if ($this->user != null && $this->user->getIsConfirmed()) {
+                    $this->addError('email', Yii::t('user', 'This account has already been confirmed'));
+                }
+            }]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'email' => Yii::t('user', 'Email')
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function formName()
+    {
+        return 'resend-form';
+    }
+
+    /**
+     * Creates new confirmation token and sends it to the user.
+     *
+     * @return boolean
+     */
+    public function resend()
+    {
+        if (!$this->validate()) {
+            return false;
+        }
+        /** @var Token $token */
+        $token = new Token(['user_id' => $this->user->id, 'type' => Token::TYPE_CONFIRMATION]);
+        $token->save(false);
+        $this->module->sendMessage($this->user->email,Yii::t('user', 'Confirm account on {0}', Yii::$app->name),'confirmation',['user' => $this->user, 'token' => $token]);
+        Yii::$app->session->setFlash('info', Yii::t('user', 'A message has been sent to your email address. It contains a confirmation link that you must click to complete registration.'));
+        return true;
+    }
+}
