9c5e100c7fd07456f3038fd9b64f112736e2e1b7
xutl <79109681@qq.com>
2016-09-19 18:11:46 +0800

    cccc

--- backend/controllers/UserController.php
+++ backend/controllers/UserController.php
@@ -6,13 +6,265 @@
  */
 namespace yuncms\user\backend\controllers;
 
+use Yii;
+use yii\helpers\Url;
+use yii\web\Response;
 use yii\web\Controller;
+use yii\widgets\ActiveForm;
+use yii\filters\VerbFilter;
+use yii\data\ActiveDataProvider;
+use yii\web\NotFoundHttpException;
+use yuncms\user\models\User;
+use yuncms\user\models\Profile;
+use yuncms\user\backend\models\UserSearch;
 
-/**
- * Class UserController
- * @package yuncms
- */
 class UserController extends Controller
 {
+    /** @inheritdoc */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['post'],
+                    'confirm' => ['post'],
+                    'block' => ['post'],
+                ],
+            ]
+        ];
+    }
 
+    /**
+     * 用户管理首页
+     */
+    public function actionIndex()
+    {
+        Url::remember('', 'actions-redirect');
+        $searchModel = Yii::createObject(UserSearch::className());
+        $dataProvider = $searchModel->search(Yii::$app->request->get());
+
+        return $this->render('index', [
+            'dataProvider' => $dataProvider,
+            'searchModel' => $searchModel,
+        ]);
+    }
+
+    /**
+     * Creates a new User model.
+     * If creation is successful, the browser will be redirected to the 'index' page.
+     *
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        /** @var User $user */
+        $user = Yii::createObject([
+            'class' => User::className(),
+            'scenario' => 'create',
+        ]);
+        $this->performAjaxValidation($user);
+        if ($user->load(Yii::$app->request->post()) && $user->create()) {
+            Yii::$app->getSession()->setFlash('success', Yii::t('user', 'User has been created'));
+            return $this->redirect(['update', 'id' => $user->id]);
+        }
+        return $this->render('create', [
+            'user' => $user,
+        ]);
+    }
+
+    /**
+     * Updates an existing User model.
+     *
+     * @param int $id
+     *
+     * @return mixed
+     */
+    public function actionUpdate($id)
+    {
+        Url::remember('', 'actions-redirect');
+        $user = $this->findModel($id);
+        $user->scenario = 'update';
+        $this->performAjaxValidation($user);
+        if ($user->load(Yii::$app->request->post()) && $user->save()) {
+            Yii::$app->getSession()->setFlash('success', Yii::t('user', 'Account details have been updated'));
+            return $this->refresh();
+        }
+        return $this->render('_account', [
+            'user' => $user,
+        ]);
+    }
+
+    /**
+     * Updates an existing profile.
+     *
+     * @param int $id
+     *
+     * @return mixed
+     */
+    public function actionUpdateProfile($id)
+    {
+        Url::remember('', 'actions-redirect');
+        $user = $this->findModel($id);
+        $profile = $user->profile;
+        if ($profile == null) {
+            $profile = Yii::createObject(Profile::className());
+            $profile->link('user', $user);
+        }
+        $this->performAjaxValidation($profile);
+        if ($profile->load(Yii::$app->request->post()) && $profile->save()) {
+            Yii::$app->getSession()->setFlash('success', Yii::t('user', 'Profile details have been updated'));
+            return $this->refresh();
+        }
+
+        return $this->render('_profile', [
+            'user' => $user,
+            'profile' => $profile,
+        ]);
+    }
+
+    /**
+     * Shows information about user.
+     *
+     * @param int $id
+     *
+     * @return string
+     */
+    public function actionView($id)
+    {
+        Url::remember('', 'actions-redirect');
+        $user = $this->findModel($id);
+
+        return $this->render('view', [
+            'user' => $user,
+        ]);
+    }
+
+    /**
+     * Shows education about user.
+     *
+     * @param int $id
+     *
+     * @return string
+     */
+    public function actionEducation($id)
+    {
+        Url::remember('', 'actions-redirect');
+        $user = $this->findModel($id);
+        $dataProvider = new ActiveDataProvider([
+            'query' => $user->getEducations(),
+        ]);
+        return $this->render('_education', [
+            'user' => $user,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Shows career about user.
+     *
+     * @param int $id
+     *
+     * @return string
+     */
+    public function actionCareer($id)
+    {
+        Url::remember('', 'actions-redirect');
+        $user = $this->findModel($id);
+        $dataProvider = new ActiveDataProvider([
+            'query' => $user->getCareers(),
+        ]);
+        return $this->render('_career', [
+            'user' => $user,
+            'dataProvider' => $dataProvider,
+        ]);
+
+    }
+
+    /**
+     * Confirms the User.
+     *
+     * @param int $id
+     *
+     * @return Response
+     */
+    public function actionConfirm($id)
+    {
+        $model = $this->findModel($id);
+        $model->confirm();
+        Yii::$app->getSession()->setFlash('success', Yii::t('user', 'User has been confirmed'));
+        return $this->redirect(Url::previous('actions-redirect'));
+    }
+
+    /**
+     * Deletes an existing User model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     *
+     * @param int $id
+     *
+     * @return mixed
+     */
+    public function actionDelete($id)
+    {
+        $model = $this->findModel($id);
+        $model->delete();
+        Yii::$app->getSession()->setFlash('success', Yii::t('user', 'User has been deleted'));
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Blocks the user.
+     *
+     * @param int $id
+     *
+     * @return Response
+     */
+    public function actionBlock($id)
+    {
+        $user = $this->findModel($id);
+        if ($user->getIsBlocked()) {
+            $user->unblock();
+            Yii::$app->getSession()->setFlash('success', Yii::t('user', 'User has been unblocked'));
+        } else {
+            $user->block();
+            Yii::$app->getSession()->setFlash('success', Yii::t('user', 'User has been blocked'));
+        }
+        return $this->redirect(Url::previous('actions-redirect'));
+    }
+
+    /**
+     * Finds the User model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     *
+     * @param int $id
+     *
+     * @return User the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        $user = User::findOne($id);
+        if ($user === null) {
+            throw new NotFoundHttpException('The requested page does not exist');
+        }
+        return $user;
+    }
+
+    /**
+     * Performs AJAX validation.
+     *
+     * @param array|\yii\base\Model $model
+     *
+     * @throws \yii\base\ExitException
+     */
+    protected function performAjaxValidation($model)
+    {
+        if (Yii::$app->request->isAjax && !Yii::$app->request->isPjax) {
+            if ($model->load(Yii::$app->request->post())) {
+                Yii::$app->response->format = Response::FORMAT_JSON;
+                echo json_encode(ActiveForm::validate($model));
+                Yii::$app->end();
+            }
+        }
+    }
 }
---
+++ backend/models/UserSearch.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+namespace yuncms\user\backend\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+
+/**
+ * UserSearch represents the model behind the search form about User.
+ */
+class UserSearch extends Model
+{
+    /** @var string */
+    public $username;
+
+    /** @var string */
+    public $email;
+
+    /** @var int */
+    public $created_at;
+
+    /** @var string */
+    public $registration_ip;
+
+
+    /** @inheritdoc */
+    public function rules()
+    {
+        return [
+            'fieldsSafe' => [['username', 'email', 'registration_ip', 'created_at'], 'safe'],
+            'createdDefault' => ['created_at', 'default', 'value' => null],
+        ];
+    }
+
+    /** @inheritdoc */
+    public function attributeLabels()
+    {
+        return [
+            'username'        => Yii::t('user', 'Username'),
+            'email'           => Yii::t('user', 'Email'),
+            'created_at'      => Yii::t('user', 'Registration time'),
+            'registration_ip' => Yii::t('user', 'Registration ip'),
+        ];
+    }
+
+    /**
+     * @param $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = User::find();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        if (!($this->load($params) && $this->validate())) {
+            return $dataProvider;
+        }
+
+        if ($this->created_at !== null) {
+            $date = strtotime($this->created_at);
+            $query->andFilterWhere(['between', 'created_at', $date, $date + 3600 * 24]);
+        }
+
+        $query->andFilterWhere(['like', 'username', $this->username])
+            ->andFilterWhere(['like', 'email', $this->email])
+            ->andFilterWhere(['registration_ip' => $this->registration_ip]);
+
+        return $dataProvider;
+    }
+}
