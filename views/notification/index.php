bc234d235af23de03092699727a49746572df98c
xuchao <85825770@qq.com>
2017-02-06 12:45:06 +0800

    更新，暂未完结

--- views/notification/index.php
+++ views/notification/index.php
@@ -7,7 +7,7 @@ use yii\widgets\ListView;
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
 $this->title = Yii::t('user', 'Notice');
-$this->params['breadcrumbs'][] = $this->title;
+//$this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="row">
 
