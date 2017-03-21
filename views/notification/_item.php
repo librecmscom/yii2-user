7d0d7c997234f51b5cac9d8971194900c17f7ec0
xuchao <85825770@qq.com>
2017-03-21 12:04:19 +0800

    update ui

--- views/notification/_item.php
+++ views/notification/_item.php
@@ -16,9 +16,6 @@ if ($typeText == null) {
     }
 }
 ?>
-?>
-
-
 <section class="stream-list-item <?= $model->status == Notification::STATUS_UNREAD ? 'not_read' : ''; ?>">
     <a href="<?=Url::to(['/user/profile/view', 'id' => $model->user_id])?>"><?=$model->user->username?></a> <?= $model->typeText; ?>
     <?php if (in_array($model->type, ['answer_question', 'follow_question', 'comment_question', 'comment_answer', 'comment_user', 'question_at'])) {
