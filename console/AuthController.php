672eb298bf333ff0b559b672191962bd87999604
xuchao <85825770@qq.com>
2017-03-23 17:08:52 +0800

    add live

--- console/AuthController.php
+++ console/AuthController.php
@@ -4,4 +4,17 @@
  * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
  * @license http://www.tintsoft.com/license/
  */
- 
+namespace yuncms\user\console;
+
+use yii\console\Controller;
+
+class AuthController extends Controller
+{
+    /**
+     * 批量刷新 AccessToken
+     */
+    public function actionRefreshAccessToken()
+    {
+
+    }
+}
