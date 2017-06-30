51761e49dd459ccc95112c4069301a3e04d4fd05
xuchao <85825770@qq.com>
2017-06-30 19:09:38 +0800

    代码URL

--- views/layouts/space.php
+++ views/layouts/space.php
@@ -158,7 +158,7 @@ $asset = AppAsset::register($this);
                 //代码
                 [
                     'label' => Yii::t('user', 'My Codes'),
-                    'url' => ['/code/space/index'],
+                    'url' => ['/code/space/started'],
                     'visible' => Yii::$app->hasModule('code'),
                     'active' => Yii::$app->controller->module->id == "code" ? true : false
                 ],
@@ -200,7 +200,7 @@ $asset = AppAsset::register($this);
                 //代码
                 [
                     'label' => Yii::t('user', 'His Codes'),
-                    'url' => ['/code/space/index', 'id' => $user->id],
+                    'url' => ['/code/space/started', 'id' => $user->id],
                     'visible' => Yii::$app->hasModule('code'),
                     'active' => Yii::$app->controller->module->id == "code" ? true : false
                 ],
