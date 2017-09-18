7ca57a59de9a67dab646c1c30ed7f02942445504
Peter <xutongle@gmail.com>
2017-09-18 10:32:36 +0800

    增加默认配置

--- migrations/M170916081721Add_defailt_settings.php
+++ migrations/M170916081721Add_defailt_settings.php
@@ -2,6 +2,7 @@
 
 namespace yuncms\user\migrations;
 
+use Yii;
 use yii\db\Migration;
 
 /**
@@ -15,6 +16,24 @@ class M170916081721Add_defailt_settings extends Migration
     public function safeUp()
     {
 
+
+        $this->batchInsert('{{%settings}}', ['type', 'section', 'key', 'value', 'active', 'created', 'modified'], [
+            ['boolean', 'user', 'enableRegistration', '0', 1, date('Y-m-d H:i:s'), date('Y-m-d H:i:s')],
+            ['boolean', 'user', 'enableMobileRegistration', '0', 1, date('Y-m-d H:i:s'), date('Y-m-d H:i:s')],
+            ['boolean', 'user', 'enableRegistrationCaptcha', '0', 1, date('Y-m-d H:i:s'), date('Y-m-d H:i:s')],
+            ['boolean', 'user', 'enableGeneratingPassword', '0', 1, date('Y-m-d H:i:s'), date('Y-m-d H:i:s')],
+            ['boolean', 'user', 'enableConfirmation', '0', 1, date('Y-m-d H:i:s'), date('Y-m-d H:i:s')],
+            ['boolean', 'user', 'enableUnconfirmedLogin', '0', 1, date('Y-m-d H:i:s'), date('Y-m-d H:i:s')],
+            ['boolean', 'user', 'enablePasswordRecovery', '0', 1, date('Y-m-d H:i:s'), date('Y-m-d H:i:s')],
+            ['integer', 'user', 'emailChangeStrategy', '1', 1, date('Y-m-d H:i:s'), date('Y-m-d H:i:s')],
+            ['integer', 'user', 'mobileChangeStrategy', '1', 1, date('Y-m-d H:i:s'), date('Y-m-d H:i:s')],
+            ['integer', 'user', 'rememberFor', '1209600', 1, date('Y-m-d H:i:s'), date('Y-m-d H:i:s')],
+            ['integer', 'user', 'confirmWithin', '86400', 1, date('Y-m-d H:i:s'), date('Y-m-d H:i:s')],
+            ['integer', 'user', 'recoverWithin', '21600', 1, date('Y-m-d H:i:s'), date('Y-m-d H:i:s')],
+            ['integer', 'user', 'cost', '10', 1, date('Y-m-d H:i:s'), date('Y-m-d H:i:s')],
+            ['string', 'user', 'avatarPath', '@root/uploads/avatar', 1, date('Y-m-d H:i:s'), date('Y-m-d H:i:s')],
+            ['string', 'user', 'avatarUrl', '@web/uploads/avatar', 1, date('Y-m-d H:i:s'), date('Y-m-d H:i:s')],
+        ]);
     }
 
     /**
@@ -22,9 +41,7 @@ class M170916081721Add_defailt_settings extends Migration
      */
     public function safeDown()
     {
-        echo "M170916081721Add_defailt_settings cannot be reverted.\n";
-
-        return false;
+        $this->delete('{{%settings}}', ['section' => 'user']);
     }
 
     /*
